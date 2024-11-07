83a58cb1a7c8f233f8efcd87939cbf1b08cc8446
arghavankb <arghavankb@gmail.com>
2024-11-07 15:25:41 +0330

    58. Length of Last Word

--- 58_length_of_last_word.py
+++ 58_length_of_last_word.py
@@ -1,6 +1,24 @@
+# First Solution
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s_strip = s.strip()
         s_split = s_strip.split(" ")
 
         return len(s_split[-1])
+
+
+
+# Second Solution
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         i = len(s)
+#         l = 0
+#
+#         while i > 0:
+#             i-=1
+#             if s[i] != " ":
+#                 l+=1
+#             elif l!=0:
+#                 break
+#
+#         return l
